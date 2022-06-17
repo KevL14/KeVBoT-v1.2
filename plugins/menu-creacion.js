42afb3f49841206c5f21e54b52ba48e72d873fde
KevL14 <99137212+KevL14@users.noreply.github.com>
2022-06-16 22:11:06 -0600

    Update menu-creacion.js

--- plugins/menu-creacion.js
+++ plugins/menu-creacion.js
@@ -10,8 +10,8 @@ let mentionedJid = [who]
 let username = conn.getName(who)
 let pp = 'https://i.imgur.com/BfsbCOR.jpg'
 let menu =`
-╭━〘 🐈⚡️🐈⚡️🐈⚡️🐈⚡️🐈 〙━╮
- ‖ ֎┉┉┉⊰ 𝙂𝙖𝙩𝙖𝘽𝙤𝙩 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 ⊱┉┉┉֍
+╭━〘 ☆ ☆ ☆ ☆ ☆ ☆ ☆ 〙━╮
+ ‖ ֎┉┉┉⊰ KeVBoT-v1.2 ⊱┉┉┉֍
  ‖ 💖 *¡𝗛ola! ${username}* 💖
  ‖    ֎┉┉┉┉┉┉⊰ 🌟 ⊱┉┉┉┉┉┉┉֍
  ‖
@@ -56,10 +56,10 @@ let menu =`
  ‖⇛ 🎨 _${usedPrefix}vibracion *cantidad*_
  ‖⇛ 🎨 _${usedPrefix}tovn *audio a nota de voz*
  ‖ ➥ ⧼ *_MENÚ DE CREACIÓN_* ⧽ 
-╰━〘 🐈🌟🐈🌟🐈🌟🐈🌟🐈 〙━╯
+╰━〘 ☆ ☆ ☆ ☆ ☆ ☆ ☆ 〙━╯
 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨`.trim()
 conn.send3ButtonLoc(m.chat, (await fetch(pp)).buffer(), `
-⁖ᯓ፨҈༺ 𝙂𝙖𝙩𝙖𝘽𝙤𝙩 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 ༻፨҈ᯓ⁖
+⁖ᯓ፨҈༺ KeVBoT-v1.2 ༻፨҈ᯓ⁖
 `.trim(), menu, '💥 𝘼𝙉𝙄𝙈𝙀/𝙍𝘼𝙉𝘿𝙊𝙈 💥', `#menurandom`, '🔞 𝙈𝙀𝙉𝙐 +18 🔞', `#labiblia`, '🔊 𝙈𝙀𝙉𝙐 𝘿𝙀 𝘼𝙐𝘿𝙄𝙊𝙎 🔊', `#menuaudio`, m, false, { contextInfo: { mentionedJid }})}
 
 handler.command = /^(menucreador|menucreacion|Menucreador|Menucreacion|Menúcreador|Menúcreacion|menúcreacion|menúcreador)$/i
